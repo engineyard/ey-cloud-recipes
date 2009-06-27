1a1e65a873723fb183c6423dd9dcf2ba9b60491d
Johnathon Hornbeck <hornbeck@alter.local>
2009-06-27 14:53:19 -0500

    adding the first iteration of the mongodb recipe

---
+++ cookbooks/mongodb/default.rb
@@ -0,0 +1,43 @@
+#
+# Cookbook Name:: mongodb
+# Recipe:: default
+
+USER = "grocio"
+
+directory "/data/db" do
+  owner USER
+  group USER
+  mode 0755
+  recursive true
+end
+  
+execute "install-mongodb" do
+  command %Q{
+    curl -O http://downloads.mongodb.org/linux/mongodb-linux-i686-0.9.5.tgz &&
+    tar zxvf mongodb-linux-i686-0.9.5.tgz &&
+    mv mongodb-linux-i686-0.9.5 /usr/local/mongodb &&
+    rm mongodb-linux-i686-0.9.5.tgz
+  }
+end
+  
+execute "add-to-path" do
+  command %Q{
+    echo 'export PATH=$PATH:/usr/local/mongodb/bin' >> /etc/profile
+  }
+  not_if "grep 'export PATH=$PATH:/usr/local/mongodb/bin' /etc/profile"
+end
+  
+execute "install-mongomapper" do
+  command %Q{
+    gem install mongomapper --no-rdoc --no-ri
+  }
+end
+  
+execute "start-mongodb" do
+  command %Q{
+    mongod run &
+  }
+  not_if "pgrep mongod"
+end  
+
+
