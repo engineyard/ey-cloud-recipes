8fefe23decb249bda08d2dae151aa33569183ca7
Ben Burkert & Wesley Beary <bburkert@engineyard.com>
2009-07-24 13:25:40 -0700

    Remove integrity cookbook

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -7,8 +7,5 @@ end
 # uncomment if you want to run couchdb recipe
 # require_recipe "couchdb"
 
-# uncomment to turn your instance into an integrity CI server
-#require_recipe "integrity"
-
 # uncomment to turn use the MBARI ruby patches for decreased memory usage and better thread/continuationi performance
 # require_recipe "mbari-ruby"
