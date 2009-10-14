b6381c67b689bd35f5ceb55554e67deb929fecc9
Alex Malinovich <amalinovich@engineyard.com>
2009-10-14 13:27:45 -0700

    Allow memcached recipe to work with solo instances

--- cookbooks/memcached/recipes/default.rb
+++ cookbooks/memcached/recipes/default.rb
@@ -8,8 +8,8 @@ node[:applications].each do |app_name,data|
   user = node[:users].first
 
 case node[:instance_role]
- when "app", "app_master"
-   template "/data/#{app_name}/shared/config/memcached.yml" do
+ when "solo", "app", "app_master"
+   template "/data/#{app_name}/shared/config/memcached_custom.yml" do
      source "memcached.yml.erb"
      owner user[:username]
      group user[:username]
