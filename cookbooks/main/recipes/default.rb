3d73af5819893753151421250d7d2198c1c56221
c3 <courtenay@entp.com>
2010-03-25 18:56:45 -0700

    wtf this doesn't exist

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -10,7 +10,7 @@
 # require_recipe "couchdb"
 
 # uncomment to turn use the MBARI ruby patches for decreased memory usage and better thread/continuationi performance
-require_recipe "mbari-ruby"
+# require_recipe "mbari-ruby"
 
 # uncomment to turn on thinking sphinx 
 require_recipe "thinking_sphinx"
