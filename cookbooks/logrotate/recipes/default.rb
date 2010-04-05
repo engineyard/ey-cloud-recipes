cc579774da5c7901d103975e3ede97d432f08619
Scott M. Likens <slikens@engineyard.com>
2010-04-05 09:39:09 -0700

    - Add Logrotate cookbook, which will add/update the nginx logrotate     file to be more flexibile and rotate if the log file is > 100M and     sets a crontab to run every hour to run this logrotate.
    Signed-off-by: Scott M. Likens <slikens@engineyard.com>

---
+++ cookbooks/logrotate/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: logrotate
+# Recipe:: default
+#
+
+remote_file "/etc/logrotate.d/nginx" do
+  source "nginx.logrotate"
+  owner "root"
+  group "root"
+  mode "0655"
+end
+
+cron "logrotate -f /etc/logrotate.d/nginx" do
+  minute  '0'
+  command "logrotate -f /etc/logrotate.d/nginx"
+  user "root"
+end
