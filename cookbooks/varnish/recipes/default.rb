1ff9ae507ffc906e021cb7d2c14d86ffed65ec94
Kirk Haines <wyhaines@gmail.com>
2010-05-07 10:27:03 -0600

    Added bash -c to my execute block for editing configs.

--- cookbooks/varnish/recipes/default.rb
+++ cookbooks/varnish/recipes/default.rb
@@ -113,7 +113,7 @@ end
 
 execute "Edit the config files inline to change nginx from listening on port 80 to listening on port 81" do
   command %Q{
-    for k in `grep -l 'listen 80;' *.conf`; do perl -p -i -e's{listen 80;}{listen 81;}' $k; mv $k "keep.$k"; done
+    bash -c 'for k in `grep -l "listen 80;" *.conf`; do perl -p -i -e"s{listen 80;}{listen 81;}" $k; mv $k "keep.$k"; done'
   }
 end
 
