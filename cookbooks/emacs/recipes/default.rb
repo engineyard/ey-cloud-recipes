6adf649960122d6fa8b27df42cd73be79323aa11
Scott M. Likens <slikens@engineyard.com>
2010-05-20 12:18:09 -0700

    - Update the emacs recipe so it works actually.
    Signed-off-by: Scott M. Likens <slikens@engineyard.com>

--- cookbooks/emacs/recipes/default.rb
+++ cookbooks/emacs/recipes/default.rb
@@ -5,14 +5,14 @@
 
 remote_file "/engineyard/portage/distfiles/eselect-emacs-1.8.tar.bz2" do
   source "http://mirror.internode.on.net/pub/gentoo-portage/distfiles/eselect-emacs-1.8.tar.bz2"
-  owner root
-  group root
-  mode 0544
+  owner "root"
+  group "root"
+  mode "0655"
   backup 0
   not_if { FileTest.exists?("/engineyard/portage/distfiles/eselect-emacs-1.8.tar.bz2") }
 end
 
 execute "install_emacs" do
-  command "emerge emacs"
+  command "emerge app-editors/emacs"
   not_if { FileTest.exists?("/usr/bin/emacs-22") }
 end
