b6a732e89f5a3b01ebe1e3efca33764ce4210194
Scott M. Likens <slikens@engineyard.com>
2010-06-09 21:40:36 -0700

    - Update resque script to place end at the proper place, as pointed     out by Daniel Nelson.  -- Thanks
    Signed-off-by: Scott M. Likens <slikens@engineyard.com>

--- cookbooks/resque/recipes/default.rb
+++ cookbooks/resque/recipes/default.rb
@@ -20,41 +20,41 @@ if ['solo', 'util'].include?(node[:instance_role])
     when 'c1.xlarge': worker_count = 8
       else 
         worker_count = 4
-      end
     end
   
 
-  node[:applications].each do |app, data|
-    template "/etc/monit.d/resque_#{app}.monitrc" do 
-    owner 'root' 
-    group 'root' 
-    mode 0644 
-    source "monitrc.conf.erb" 
-    variables({ 
-    :num_workers => worker_count,
-    :app_name => app, 
-    :rails_env => node[:environment][:framework_env] 
-    }) 
-    end
-
-    worker_count.times do |count|
-      template "/data/#{app}/shared/config/resque_#{count}.conf" do
-      owner node[:owner_name]
-      group node[:owner_name]
-      mode 0644
-      source "resque_wildcard.conf.erb"
+    node[:applications].each do |app, data|
+      template "/etc/monit.d/resque_#{app}.monitrc" do 
+      owner 'root' 
+      group 'root' 
+      mode 0644 
+      source "monitrc.conf.erb" 
+      variables({ 
+      :num_workers => worker_count,
+      :app_name => app, 
+      :rails_env => node[:environment][:framework_env] 
+      }) 
       end
+
+      worker_count.times do |count|
+        template "/data/#{app}/shared/config/resque_#{count}.conf" do
+        owner node[:owner_name]
+        group node[:owner_name]
+        mode 0644
+        source "resque_wildcard.conf.erb"
+        end
+      end
+
+    execute "ensure-resque-is-setup-with-monit" do 
+      command %Q{ 
+      monit reload 
+      } 
     end
 
-  execute "ensure-resque-is-setup-with-monit" do 
-    command %Q{ 
-    monit reload 
-    } 
-  end
-
-  execute "restart-resque" do 
-    command %Q{ 
-      echo "sleep 20 && monit -g #{app}_resque restart all" | at now 
-    } 
+    execute "restart-resque" do 
+      command %Q{ 
+        echo "sleep 20 && monit -g #{app}_resque restart all" | at now 
+      }
+    end
   end 
 end
