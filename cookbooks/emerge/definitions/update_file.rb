31871fca00c35f654effed239e1f7169dd065592
Scott M. Likens <slikens@engineyard.com>
2010-07-01 14:20:01 -0700

    - Emerge Cookbook, making unmasking packages easier.     - See README for specifics.
    Signed-off-by: Scott M. Likens <slikens@engineyard.com>

---
+++ cookbooks/emerge/definitions/update_file.rb
@@ -0,0 +1,34 @@
+define :update_file, :action => :append do
+  require 'tempfile'
+
+  filepath = params[:path] || params[:name]
+
+  file params[:name] do
+    backup params[:backup] if params[:backup]
+    group params[:group] if params[:group]
+    mode params[:mode] if params[:mode]
+    owner params[:owner] if params[:owner]
+    path filepath
+  end
+
+  case params[:action].to_sym
+  when :append, :rewrite
+
+    mode = params[:action].to_sym == :append ? 'a' : 'w'
+
+    ruby_block "#{params[:action]}-to-#{filepath}" do
+      block do
+        File.open(filepath, mode) do |f|
+          f.puts params[:body]
+        end
+      end
+    end
+
+  when :truncate
+
+    execute "truncate-#{filepath}" do
+      command "> #{filepath}"
+    end
+
+  end
+end
