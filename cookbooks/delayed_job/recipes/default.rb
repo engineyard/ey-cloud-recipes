3342b779dde4ad599c050274ee0bea3cf201c6c5
bratta <tgourley@gmail.com>
2010-08-09 08:28:31 -0500

    Adding delayed job recipe

---
+++ cookbooks/delayed_job/recipes/default.rb
@@ -0,0 +1,43 @@
+#
+# Cookbook Name:: delayed_job
+# Recipe:: default
+#
+
+if ['solo', 'util'].include?(node[:instance_role]) && !node[:name].match(/^mongodb_/)
+  node[:applications].each do |app_name,data|
+  
+    # determine the number of workers to run based on instance size
+    if node[:instance_role] == 'solo'
+      worker_count = 1
+    else
+      case node[:ec2][:instance_type]
+      when 'm1.small': worker_count = 2
+      when 'c1.medium': worker_count = 4
+      when 'c1.xlarge': worker_count = 8
+      else 
+        worker_count = 2
+      end
+    end
+    
+    worker_count.times do |count|
+      template "/etc/monit.d/delayed_job#{count+1}.#{app_name}.monitrc" do
+        source "dj.monitrc.erb"
+        owner "root"
+        group "root"
+        mode 0644
+        variables({
+          :app_name => app_name,
+          :user => node[:owner_name],
+          :worker_name => "delayed_job#{count+1}",
+          :framework_env => node[:environment][:framework_env]
+        })
+      end
+    end
+    
+    execute "monit-reload-restart" do
+       command "sleep 30 && monit quit"
+       action :run
+    end
+      
+  end
+end
