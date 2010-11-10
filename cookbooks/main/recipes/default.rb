42401ea046c7d7a23d087ab27ca07a7decaa6927
Scott M. Likens <slikens@engineyard.com>
2010-11-10 11:17:13 -0800

    - Update references to smtp.sendgrid.com to .net to be proper.
      Signed-off-by: Scott M. Likens <slikens@engineyard.com>

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -37,7 +37,7 @@
 #uncomment to run the exim recipe
 #exim_auth "auth" do
 #  my_hostname "my_hostname.com"
-#  smtp_host "smtp.sendgrid.com"
+#  smtp_host "smtp.sendgrid.net"
 #  username "username"
 #  password "password"
 #end
