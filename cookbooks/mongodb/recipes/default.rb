3e82941d6fc027f3f3b2d0dd40dde6a744189775
Scott M. Likens <slikens@engineyard.com>
2011-04-28 10:33:48 -0700

    - Fix it so the attributes no longer error out when no utility     instances are booted.   - Make it so that it does not run on 32-bit platforms as that can     damage your data.
    Signed-off-by: Scott M. Likens <slikens@engineyard.com>

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -4,21 +4,23 @@
 #
 
 # Setup an arbiter on the db_master|solo as replica sets need another vote to properly failover.  If you have a Replica set > 3 nodes we don't set this up, you can tune this obviously.
-
-if (['db_master','solo'].include?(@node[:instance_role]) &&  @node[:mongo_utility_instances].length < 3)
-  require_recipe "mongodb::install"
-  require_recipe "mongodb::configure"
-  require_recipe "mongodb::start"
-end
-
-if (@node[:instance_role] == 'util' && @node[:name].match(/mongodb/)) || (@node[:instance_role] == "solo" &&  @node[:mongo_utility_instances].length == 0)
-
-  require_recipe "mongodb::install"
-  require_recipe "mongodb::configure"
-  require_recipe "mongodb::start"
-
-  if @node[:mongo_replset]
-    require_recipe "mongodb::replset"
+case node[:kernel][:machine]
+when "i686"
+  # Do nothing, you should never run MongoDB in a i686/i386 environment it will damage your data.
+else
+  if (['db_master','solo'].include?(@node[:instance_role]) &&  @node[:mongo_utility_instances].length < 3)
+    require_recipe "mongodb::install"
+    require_recipe "mongodb::configure"
+    require_recipe "mongodb::start"
+  end
+  
+  if (@node[:instance_role] == 'util' && @node[:name].match(/mongodb/)) || (@node[:instance_role] == "solo" &&  @node[:mongo_utility_instances].length == 0)
+    require_recipe "mongodb::install"
+    require_recipe "mongodb::configure"
+    require_recipe "mongodb::start"
+    if @node[:mongo_replset]
+      require_recipe "mongodb::replset"
+    end
   end
 end
 
