a6b5195f2b0c2d02c982624d91d5c4760593e4e6
Hiro Asari <asari.ruby@gmail.com>
2011-05-02 11:01:37 -0700

    Remove superfluous 'h' in the previous commit.

--- cookbooks/delayed_job/recipes/default.rb
+++ cookbooks/delayed_job/recipes/default.rb
@@ -35,7 +35,7 @@ if node[:instance_role] == "solo" || (node[:instance_role] == "util" && node[:na
     end
     
     execute "monit-reload-restart" do
-       command "sleep 30 && monit reload && monit restart all dj_#{app_name}h"
+       command "sleep 30 && monit reload && monit restart all dj_#{app_name}"
        action :run
     end
       
