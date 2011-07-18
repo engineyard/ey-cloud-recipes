2cc7d0646d89f85935fbeb51c37aa469dd1d8ea8
Ines Sombra <isombra@engineyard.com>
2011-07-18 14:01:32 -0700

    changes to auto explain

--- cookbooks/postgresql9_contrib/recipes/ext.rb
+++ cookbooks/postgresql9_contrib/recipes/ext.rb
@@ -2,14 +2,14 @@
 # config_options.each { |key, value|  key + ' = ' + value + "\n" }
 Chef::Log.info "Config file to be modified #{config_options.inspect()}"
 
-template "#{postgres_root}/#{postgres_version}/custom.conf" do
+template "#{node[:postgres_root]}#{node[:postgres_version]}/custom.conf" do
   source "custom.erb"
   owner "postgres"
   group "root"
   mode 0600
   backup 0
   # notifies :restart, resources(:service => "postgresql-#{postgres_version}")
-  variables(config_options )
+  variables( )
 end
 
 
---
+++ cookbooks/postgresql9_contrib/recipes/ext_autoexplain.rb
@@ -0,0 +1,33 @@
+# ---- Modify pgsql config file ----
+Chef::Log.info "-- from ext_autoexplain --" 
+
+template "#{@node[:postgres_root]}#{@node[:postgres_version]}/custom_autoexplain.conf" do
+  source "custom_autoexplain.erb"
+  owner "postgres"
+  group "root"
+  mode 0600
+  backup 0
+  notifies :restart, resources(:service => "postgresql-#{@node[:postgres_version]}")
+  variables({
+    :shared_preload_libraries => "'auto_explain'",
+    :custom_variable_classes => "'auto_explain'",
+    :auto_explain_log_min_duration => "'3s'",
+    :auto_explain_log_analyze => "'false'",
+    :auto_explain_log_verbose => "'false'",
+    :auto_explain_log_buffers => "'false'",
+    :auto_explain_log_format =>  "'text'",
+    :auto_explain_log_nested_statements => "'false'"
+  })
+end
+
+
+# ---- Execute sql ----
+# Chef::Log.info "Execute sql" 
+if #{sql_per_db}.empty() 
+  Chef::Log.info "No sql to execute"
+else
+  Chef::Log.info "executing custom sql for extension"
+
+end
+  
+  
