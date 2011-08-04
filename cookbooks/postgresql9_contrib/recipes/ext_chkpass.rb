f93d2c6e36c44971c732e04137d002da81ed0cdb
Ines Sombra <isombra@engineyard.com>
2011-08-04 00:19:52 -0500

    autoexplain & chkpass
    fixed errors on auto explain & chkpass

--- cookbooks/postgresql9_contrib/recipes/ext_chkpass.rb
+++
@@ -1,8 +0,0 @@
-Chef::Log.info "-- from ext_chkpass --" 
-
-  execute "enable chpass on db" do
-    command "psql -d #{dbname_to_use} -f /usr/share/postgresql-9.0/contrib/chkpass.sql"
-  end
-
-  
-  
