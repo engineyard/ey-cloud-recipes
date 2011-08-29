b626197cc151cd140913fa520feb48533247352b
Ines Sombra <isombra@engineyard.com>
2011-08-29 16:44:39 -0700

    Changing things in pg contribs

--- cookbooks/postgresql9_contrib/definitions/postgres_loaders.rb
+++ cookbooks/postgresql9_contrib/definitions/postgres_loaders.rb
@@ -1,12 +1,10 @@
-# define a loader for postgres sql files
-module PostgresFunctionLoaders
-  def load_sql_file(db_name, contrib_path)
-   command "psql -U postgres -d #{db_name} -f #{contrib_path}"
-  end
-  
-  def load_shared_library(db_name, library_name)
-   command "psql -U postgres -d #{db_name} -c \"LOAD \'#{library_name}\'\";"
-  end
-  
+# define loader functions for postgres sql files or libraries
+
+def load_sql_file(db_name, contrib_path)
+ command "psql -U postgres -d #{db_name} -f #{contrib_path}"
 end
- 
+
+def load_shared_library(db_name, library_name)
+ command "psql -U postgres -d #{db_name} -c \"LOAD \'#{library_name}\'\";"
+end
+
