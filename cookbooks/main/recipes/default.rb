e27fa22cfd6dca7539ddcf57dc62ea5aed67e265
Ines Sombra <isombra@engineyard.com>
2011-10-09 19:53:42 -0700

    added hstore recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -63,6 +63,7 @@ if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
   # postgresql9_cube "postgres_test"
   # postgresql9_dblink "postgres_test"
   # postgresql9_earthdistance "postgres_test"
-  postgresql9_fuzzystrmatch "postgres_test"
+  # postgresql9_fuzzystrmatch "postgres_test"
+  postgresql9_hstore "postgres_test"
   
 end
