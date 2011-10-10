b6cd475d789fa25b26f37455e0f242594dfac1e3
Ines Sombra <isombra@engineyard.com>
2011-10-09 19:33:13 -0700

    added recipies for dblink and earthdistance

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -59,7 +59,9 @@ if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
   # postgresql9_autoexplain "postgres_test"
   # postgresql9_chkpass "postgres_test"
   # postgresql9_postgis "postgres_test"
-  postgresql9_citext "postgres_test"
+  # postgresql9_citext "postgres_test"
   # postgresql9_cube "postgres_test"
+  # postgresql9_dblink "postgres_test"
+  postgresql9_earthdistance "postgres_test"
   
 end
---
+++ cookbooks/postgresql9_contrib/definitions/earthdistance.rb
@@ -0,0 +1,10 @@
+define :postgresql9_earthdistance do
+ dbname_to_use = params[:name]  
+ 
+  load_sql_file do 
+    db_name dbname_to_use
+    username "postgres"
+    filename "/usr/share/postgresql-9.0/contrib/earthdistance.sql"
+  end
+
+end
