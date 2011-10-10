32961fdde369c5c061e80f07ae64ad13587b7ff6
Ines Sombra <isombra@engineyard.com>
2011-10-09 22:05:37 -0700

    added uuid recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -70,14 +70,14 @@ if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
   # postgresql9_ltree "postgres_test"
   # postgresql9_pg_buffercache "postgres_test" - Not done
   # postgresql9_pg_freespacemap "postgres_test" - Not done
-  postgresql9_pg_stat_statements"postgres_test"
-  # postgresql9_pg_trgm "postgres_test"
-  # postgresql9_pgcrypto "postgres_test"
-  # postgresql9_pgrowlocks "postgres_test"
-  # postgresql9_postgis "postgres_test"
+  # postgresql9_pg_stat_statements"postgres_test" - Not done
+  # postgresql9_pg_trgm "postgres_test" - Not done
+  # postgresql9_pgcrypto "postgres_test" - Not done
+  # postgresql9_pgrowlocks "postgres_test" - Not done
+  # postgresql9_postgis "postgres_test" - Not done
   # postgresql9_seg "postgres_test"
-  # postgresql9_tablefunc "postgres_test"
-  # postgresql9_unaccent "postgres_test"
-  # postgresql9_uuid_ossp "postgres_test"
+  # postgresql9_tablefunc "postgres_test" - Not done
+  # postgresql9_unaccent "postgres_test" - Not done
+  postgresql9_uuid_ossp "postgres_test"
   
 end
