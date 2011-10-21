b62b8e7df653a5b5055bf144f674bba661639c88
Tyler Bird <tyler.bird@gmail.com>
2011-10-20 17:15:42 -0700

    made delayed_job monit config increment process name

--- cookbooks/delayed_job/recipes/default.rb
+++ cookbooks/delayed_job/recipes/default.rb
@@ -26,6 +26,7 @@ if node[:instance_role] == "solo" || (node[:instance_role] == "util" && node[:na
         group "root"
         mode 0644
         variables({
+          :num_workers => worker_count,
           :app_name => app_name,
           :user => node[:owner_name],
           :worker_name => "delayed_job#{count+1}",
