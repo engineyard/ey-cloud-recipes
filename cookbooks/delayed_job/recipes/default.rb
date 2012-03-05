01b44e572356e3c824d4f7df656447ad28c6d9ad
Christopher Rigor <crigor@gmail.com>
2012-03-05 19:15:10 +0800

    add the app name to the worker name

--- cookbooks/delayed_job/recipes/default.rb
+++ cookbooks/delayed_job/recipes/default.rb
@@ -28,7 +28,7 @@ if node[:instance_role] == "solo" || (node[:instance_role] == "util" && node[:na
         variables({
           :app_name => app_name,
           :user => node[:owner_name],
-          :worker_name => "delayed_job#{count+1}",
+          :worker_name => "#{app_name}_delayed_job#{count+1}",
           :framework_env => node[:environment][:framework_env]
         })
       end
