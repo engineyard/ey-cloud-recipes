6b1d094305d0ba02022f4b3c51e3b55043636391
Evan Machnic <emachnic@broadmac.net>
2012-03-10 17:53:19 -0500

    Add example emerge recipe with documentation

---
+++ cookbooks/emerge/recipes/default.rb
@@ -0,0 +1,21 @@
+#############################################
+# Sample recipe for emerging packages
+# 
+# Search the Engine Yard portage tree to find
+# out package versions to install
+#
+# EXAMPLE:
+# Search for libxml2
+# $ eix libxml2
+#############################################
+
+# Unmask version 2.7.6 of libxml2
+# enable_package "dev-libs/libxml2" do
+#   version "2.7.6"
+# end
+
+# Install the newly unmasked version
+# package "dev-libs/libxml2" do
+#   version "2.7.6"
+#   action :install
+# end
