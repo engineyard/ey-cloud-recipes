7fdcdec60d7cd332c824e4ddd0893f8bad47e7d3
Barry Gordon <barry.gordon@examtime.com>
2012-07-20 17:05:09 +0100

    Add delayed job strategies for Generic and Named worker groups

--- cookbooks/delayed_job/recipes/default.rb
+++ cookbooks/delayed_job/recipes/default.rb
@@ -3,26 +3,94 @@
 # Recipe:: default
 #
 
+WorkerRole = Struct.new(:count, :queue)
+
+class GenericWorkerStrategy
+
+  def self.generate(node)
+    if node[:instance_role] == 'solo'
+      Chef::Log.info "Delayed Job being configured for a solo instance"
+      strategy = self.for_solo
+    else
+      strategy = self.for_cluster node[:instance_role], node[:ec2][:instance_type]
+    end
+    log strategy, node
+    return strategy
+  end
+  
+  def self.for_solo
+    [ WorkerRole.new(1, nil) ]
+  end
+  
+  def self.for_cluster(role, instance_type)
+    case instance_type
+      when 'm1.small'
+        worker_count = 1
+      when 'c1.medium'
+        worker_count = 2
+      when 'c1.xlarge'
+        worker_count = 4
+      else
+        worker_count = 2
+    end
+    
+    [ WorkerRole.new(worker_count, nil) ]
+  end
+  
+  def self.log(strategy, node)
+    strategy.each do | role |
+      if role.queue.nil?
+        Chef::Log.info "Delayed Job worker count has been set to '#{role.count}' as this is an EC2 instance of size #{node[:ec2][:instance_type]}"
+      else
+        Chef::Log.info "Delayed Job worker count has been set to '#{role.count}' for queue '#{role.queue}' as this is an EC2 instance of size #{node[:ec2][:instance_type]}"
+      end
+    end
+  end
+
+end
+
+class NamedWorkerStrategy < GenericWorkerStrategy
+  
+  def self.for_solo
+    [ 
+      WorkerRole.new(1, 'reporting'),
+      WorkerRole.new(1, 'util'),
+      WorkerRole.new(1, 'mail')
+    ]
+  end
+  
+  def self.for_cluster(role, instance_type)
+    case instance_type
+      when 'm1.small'
+        reporting_count = 1 if role == 'app_master'
+        util_count = 1 if role == 'app'
+        mail_count = 1
+      when 'c1.medium'
+        reporting_count = 1 if role == 'app_master'
+        util_count = 1 if role == 'app'
+        mail_count = 2
+      when 'c1.xlarge'
+        reporting_count = 1 if role == 'app_master'
+        util_count = 2 if role == 'app'
+        mail_count = 4
+      else
+        reporting_count = 1 if role == 'app_master'
+        util_count = 1 if role == 'app'
+        mail_count = 2
+    end
+    
+    [ 
+      WorkerRole.new(reporting_count, 'reporting'),
+      WorkerRole.new(util_count, 'util'),
+      WorkerRole.new(mail_count, 'mail')
+    ]
+  end
+  
+end
+
 if ( %w(solo app_master app).include?(node[:instance_role]) || (node[:instance_role] == 'util' && node[:name] !~ /^(mongodb|redis|memcache)/) )
 
   node[:applications].each do |app_name,data|
-
-    if node[:instance_role] == 'solo'
-      Chef::Log.info "Delayed Job being configured for a solo instance"
-      worker_count = 1
-    else
-      case node[:ec2][:instance_type]
-        when 'm1.small'
-          worker_count = 1
-        when 'c1.medium'
-          worker_count = 2
-        when 'c1.xlarge'
-          worker_count = 4
-        else
-          worker_count = 2
-      end
-    end
-    Chef::Log.info "Delayed Job worker count has been set to '#{worker_count}' as this is an EC2 instance of size #{node[:ec2][:instance_type]}"
 
     template "/etc/monit.d/#{app_name}_delayed_jobs.monitrc" do
       source "dj.monitrc.erb"
@@ -30,7 +98,7 @@ if ( %w(solo app_master app).include?(node[:instance_role]) || (node[:instance_r
       group "root"
       mode 0644
       variables({
-        :num_workers => worker_count,
+        :worker_roles => GenericWorkerStrategy.generate(node),
         :app_name => app_name,
         :app_dir => "/data/#{app_name}/current",
         :pid_dir => "/data/#{app_name}/current/tmp/pids",
