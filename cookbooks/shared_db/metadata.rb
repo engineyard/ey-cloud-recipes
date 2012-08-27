b3ea5f7a4525760c3f2c0190e8db301979002e80
Evan Machnic <emachnic@broadmac.net>
2012-08-27 11:36:19 -0400

    Added recipe to share database.yml between applications

---
+++ cookbooks/shared_db/metadata.rb
@@ -0,0 +1,5 @@
+description 'Symlink the database.yml file from one app to another'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer 'Engine Yard'
+maintainer_email 'info@engineyard.com'
+version '0.0.1'
