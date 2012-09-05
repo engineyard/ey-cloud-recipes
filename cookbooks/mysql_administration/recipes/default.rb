319bc24ab5c54d1871b5359d86566c512f21bdf4
Tyler Poland <tyler.poland@gmail.com>
2012-09-05 16:22:44 -0400

    removed extraneous and unused default.rb

--- cookbooks/mysql_administration/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: mysql_administration 
-# Recipe:: default
-#
-if ['db_slave'].include?(node[:instance_role])
-  if skip_slave_binlog
-    require_recipe "mysql_administration::skip_slave_binlog"
-  end
-end
