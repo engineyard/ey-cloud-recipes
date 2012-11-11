cb785ed18a5a00c288a72fe3d263bb73c7687e8b
Leon Xiang <leon@e-channel.com.au>
2012-11-12 10:29:28 +1030

    Sepecify queue settings for individual worker

--- cookbooks/resque/recipes/default.rb
+++ cookbooks/resque/recipes/default.rb
@@ -12,13 +12,13 @@ if ['util'].include?(node[:instance_role]) && node[:name] =~ /^Worker/
   case node[:ec2][:instance_type]
   when 'm1.small' then worker_count = 2
   when 'c1.medium'then worker_count = 8
-  when 'c1.xlarge' then worker_count = 24
+  when 'c1.xlarge' then worker_count = (node[:environment][:framework_env] == 'production' ? 24 : 8)
   else worker_count = 4
   end
 
   redis_instance = node['utility_instances'].find { |instance| instance['name'] == 'redis' }
 
-  node[:applications].each do |app, data|
+  node[:applications].reject{ |app, _| app != 'dynamiccreative' }.each do |app, data|
     template "/etc/monit.d/resque_#{app}.monitrc" do
       owner 'root'
       group 'root'
@@ -43,17 +43,20 @@ if ['util'].include?(node[:instance_role]) && node[:name] =~ /^Worker/
       })
     end
 
-    setting = (worker_count == 24 ? settings['production'] : settings['staging'])
+    setting = node[:settings][node[:environment][:framework_env]]
 
+    num = 0
     setting.each do |count, queue|
       count.times do
-        template "/data/#{app}/shared/config/resque_#{count}.conf" do
+        template "/data/#{app}/shared/config/resque_#{num}.conf" do
           owner node[:owner_name]
           group node[:owner_name]
           mode 0644
           variables({:queue => queue.join(',')})
           source "resque_wildcard.conf.erb"
         end
+
+        num += 1
       end
     end
 
