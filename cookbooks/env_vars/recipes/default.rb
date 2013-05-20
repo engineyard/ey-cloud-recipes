85371bc3711150a5d2ea2d387769f7ee5116cd8f
Matt Dolian <matt.dolian@gmail.com>
2013-05-20 15:37:17 -0400

    fixes: only reload nginx if needed, only write out ruby wrapper for passenger

--- cookbooks/env_vars/recipes/default.rb
+++ cookbooks/env_vars/recipes/default.rb
@@ -4,6 +4,11 @@
 #
 
 if ['solo', 'app', 'app_master', 'util'].include?(node[:instance_role])
+
+  execute "reload-nginx" do
+    action :nothing
+    command "/etc/init.d/nginx reload"
+  end
   
   node[:applications].each do |app_name, data|
     template "/data/#{app_name}/shared/config/env.custom" do
@@ -15,24 +20,23 @@ if ['solo', 'app', 'app_master', 'util'].include?(node[:instance_role])
           :env_vars => node[:env_vars]
       })
     end
-    template "/data/#{app_name}/shared/bin/ruby_wrapper" do
-      source "ruby_wrapper.erb"
-      owner node[:users].first[:username]
-      group node[:users].first[:username]
-      mode 0755
-      variables({
-          :app_name => app_name
-      })
-    end
     
     case node[:environment][:stack]
     when /nginx_passenger/i
+      template "/data/#{app_name}/shared/bin/ruby_wrapper" do
+        source "ruby_wrapper.erb"
+        owner node[:users].first[:username]
+        group node[:users].first[:username]
+        mode 0755
+        variables({
+            :app_name => app_name
+        })
+      end
+            
       execute "update-nginx-passenger-ruby" do
         command "echo 'passenger_ruby /data/#{app_name}/shared/bin/ruby_wrapper;' >> /etc/nginx/stack.conf"
         not_if "grep passenger_ruby /etc/nginx/stack.conf"
-      end
-      execute "reload-nginx" do
-        command "/etc/init.d/nginx reload"
+        notifies :run, resources(:execute => "reload-nginx")
       end
     end
     
