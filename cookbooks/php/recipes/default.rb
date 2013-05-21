c739403b97ec55b1311f1bb12674389860e6e3a8
Jim Neath <jimneath@googlemail.com>
2013-05-21 09:38:56 +0100

    [CC-377] Remove php recipe

--- cookbooks/php/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: php
-# Recipe:: default
-#
-# Uncomment the following if modifying php.ini
-# include_recipe "php::ini_setup"
-
-# Make sure to specify the correct root path in the php::nginx recipe
-include_recipe "php::nginx"
