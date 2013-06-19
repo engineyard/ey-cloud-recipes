1165780f79d3a49e025c07aececabea5ac0c3a06
Domhnall Murphy <domhnall.murphy@examtime.com>
2013-06-19 13:05:54 +0100

    Extract app_name from node properties.

--- cookbooks/unicorn_custom/recipes/default.rb
+++ cookbooks/unicorn_custom/recipes/default.rb
@@ -2,25 +2,27 @@
 # Cookbook Name:: unicorn_custom
 # Recipe:: default
 #
-Chef::Log.info "Apply custom configuration for unicorn"
+node[:applications].each do |app_name, data|
+  Chef::Log.info "Apply custom configuration for unicorn on #{app_name}"
 
-if ['app', 'app_master', 'solo'].include? node[:instance_role]
+  if ['app', 'app_master', 'solo'].include? node[:instance_role]
 
-  execute "restart unicorn" do
-    command "monit restart unicorn_master_examtime"
-    action :nothing
+    execute "restart unicorn" do
+      command "monit restart unicorn_master_#{app_name}"
+      action :nothing
+    end
+
+    template "/data/#{app_name}/shared/config/env.custom" do
+      owner node[:owner_name]
+      group node[:owner_name]
+      mode 0644
+      source "env_custom"
+      notifies :run, resources(:execute => "restart unicorn")
+      variables({
+        :app_name => app_name
+      })
+    end
+
+    Chef::Log.info "Applied custom unicorn config to #{node[:instance_role]} instance"
   end
-
-  template "/data/#{app_name}/shared/config/env.custom" do
-    owner node[:owner_name]
-    group node[:owner_name]
-    mode 0644
-    source "env_custom"
-    notifies :run, resources(:execute => "restart unicorn")
-    variables({
-      :app_name => app_name
-    })
-  end
-
-  Chef::Log.info "Applied custom unicorn config to #{node[:instance_role]} instance"
 end
