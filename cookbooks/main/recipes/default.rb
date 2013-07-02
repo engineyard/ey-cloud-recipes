d750ba20214c2555a5eac9a61f1e1e7410a1abb1
engel lee <engel0088@gmail.com>
2013-07-03 06:47:49 +0800

    comment include_recipe as default

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -3,7 +3,7 @@
 #    echo "i ran at #{Time.now}" >> /root/cheftime
 #  }
 #end
-require_recipe "imageick"
+#include_recipe "imageick"
 # uncomment to turn on thinking sphinx/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
 # include_recipe "sphinx"
 
