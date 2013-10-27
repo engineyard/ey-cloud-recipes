4f20c368f027185fc9e2a738ca265b30be4b76ca
Elliot Cohen <elliot.cohen@gmail.com>
2013-10-27 02:56:14 -0400

    temp update to redis yml recipe

--- cookbooks/redis-yml/recipes/default.rb
+++ cookbooks/redis-yml/recipes/default.rb
@@ -1,7 +1,7 @@
-if ['app_master', 'app'].include?(node[:instance_role])
+if !['app_master', 'app'].include?(node[:instance_role])
 
   # If you have only one utility instance uncomment the line below
-  #redis_instance = node['utility_instances'].first
+  redis_instance = node['utility_instances'].first
   # Otherwise, if you have multiple utility instances you can specify it by uncommenting the line below
   # You can change the name of the instance based on whatever name you have chosen for your instance.
   #redis_instance = node['utility_instances'].find { |instance| instance['name'] == 'redis' }
