ece21f5d05aa80a8f4a4131e6d9c02064263d86c
rbankston <rbankston@engineyard.com>
2014-05-29 13:45:10 -0400

    Removed the Magento recipe

--- cookbooks/magento/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-default[:magento] = {
-  :encryption_key => "INSERT KEY HERE",   # Add your encryption key here
-  :redis_session_store => true,          # Set to true to enable Redis session storage
-  :redis_page_caching => true            # Set to true to enable Redis page caching
-}
--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -108,9 +108,6 @@
 
 # include_recipe "eventmachine"
 
-#uncomment to include the Magento recipe
-#include_recipe "magento"
-
 #enable Extension modules for a given Postgresql database
 # if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
   # Extensions that support Postgres >= 9.0
@@ -133,10 +130,10 @@
   # postgresql9_pg_trgm "dbname"
   # postgresql9_pgcrypto "dbname"
   # postgresql9_pgrowlocks "dbname"
-  
+
   # PostGis 1.5 (use with versions 9.0, 9.1, 9.2)
   # postgresql9_postgis "dbname"
-  
+
   # PostGis 2.0 (use with versions >= 9.2)
   #postgresql9_postgis2 "dbname"
   # postgresql9_seg "dbname"
@@ -145,15 +142,15 @@
   # postgresql9_test_parser "dbname"
   # postgresql9_unaccent "dbname"
   # postgresql9_uuid_ossp "dbname"
-  
-  
+
+
   # 9.1 and 9.2 Extensions
   # postgresql9_file_fdw "dbname"
   # postgresql9_xml2 "dbname"
-  
+
   #9.2 Extensions
   # postgresql9_pg_stat_statements "dbname"
-  
+
   # Admin-Level Contribs
   # postgresql9_pg_buffercache "postgres"
   # postgresql9_pg_freespacemap "postgres"
