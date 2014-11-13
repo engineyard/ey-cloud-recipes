cd14620578e9a5ef3a0859d7192a4e43ad54d507
Will Luongo <me@willluongo.com>
2014-11-12 19:09:52 -0500

    Add deny-directories recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -4,6 +4,9 @@
 #  }
 #end
 
+# uncomment to deny access to /log, /config, and .git directories as well as any .yml files
+# include_recipe "deny-directories"
+
 # uncomment to turn on thinking sphinx 2/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
 # include_recipe "sphinx"
 
