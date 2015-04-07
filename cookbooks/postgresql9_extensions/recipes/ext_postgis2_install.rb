00d7370aba94835f5d49964a0159364656b02e6c
Erik Jones <mage2k@gmail.com>
2015-04-07 11:25:17 -0700

    Fix Postgis recipes
    * postgis 2.1.5 for pg 9.3/9.4 on 12.11
    * Add ey_release_version helper method in main cookbook's libraries
    * Fix postgis 2.1.1 installs on 9.3/2009a and 9.2/12.11
    * Tweaks to abort execution if chosen postgis version isn't supported
      for current Postgres version and/or environment stack

--- cookbooks/postgresql9_extensions/recipes/ext_postgis2_install.rb
+++ cookbooks/postgresql9_extensions/recipes/ext_postgis2_install.rb
@@ -1,8 +1,19 @@
 if @node[:postgres_version] >= 9.2
-  postgis_version = "2.1.1"
   proj_version = "4.6.1"
   geos_version = "3.4.2"
-  gdal_version = "1.10.0-r1"
+  python_exec_version = "0.2"
+
+  if ey_release_version == '2009a'
+    gdal_version = "1.10.0"
+    postgis_version = "2.1.1-r1"
+
+    enable_package "dev-libs/json-c" do
+      version "0.11"
+    end
+  else
+    gdal_version = "1.10.0-r1"
+    postgis_version = "2.1.5"
+  end
 
   package_use "sci-libs/geos" do
     flags "-ruby"
@@ -23,6 +34,11 @@ if @node[:postgres_version] >= 9.2
     version postgis_version
   end
 
+  enable_package "dev-python/python-exec" do
+    version python_exec_version
+  end
+
+  # normal install case
   package "dev-db/postgis" do
     version postgis_version
     action :install
