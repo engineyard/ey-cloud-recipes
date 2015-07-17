409caa3b517abb41558c81e07d717c8b26c341e9
Radamanthus Batnag <radamanthus@gmail.com>
2015-07-17 13:45:17 +0800

    Use equality to determine the matching node name
    This avoids a false match on unnamed instances like app_master and db_master

--- cookbooks/cron/recipes/default.rb
+++ cookbooks/cron/recipes/default.rb
@@ -4,7 +4,7 @@
 #
 
 # Find all cron jobs specified in attributes/cron.rb where current node name matches instance_name
-crons = node[:custom_crons].find_all {|c| c[:instance_name].match "#{node[:name]}" }
+crons = node[:custom_crons].find_all {|c| c[:instance_name] == "#{node[:name]}" }
 
 crons.each do |cron|
   cron cron[:name] do
