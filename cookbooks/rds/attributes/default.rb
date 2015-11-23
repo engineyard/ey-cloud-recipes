45fd499c979db688a9653982b07650e2bc9183f6
Christopher Rigor <crigor@gmail.com>
2015-11-23 13:57:14 +0800

    Use node[:applications] and add examples to the rds cookbook

--- cookbooks/rds/attributes/default.rb
+++ cookbooks/rds/attributes/default.rb
@@ -1,13 +1,24 @@
 # place database credentials in this file
 #
-#   default[app_name] = { :adapter => '', :database => '', :username => '', :password => '', :host => '' }
+#   default[:app_name] = { :adapter => '', :database => '', :username => '', :password => '', :host => '' }
 #
 # for example:
 #
-#   default[:todo] = { 
-#     :adapter => 'mysql', 
-#     :database => 'todoproduction', 
-#     :username => 'engineyard', 
-#     :password => 'mypassword', 
-#     :host => 'todo.dhvg1ytl1sd8.us-east-1.rds.amazonaws.com' 
-#   }
+#  case node[:environment][:name]
+#  when 'env1'
+#    default[:todo] = { 
+#      :adapter => 'mysql', 
+#      :database => 'todoproduction', 
+#      :username => 'engineyard', 
+#      :password => 'mypassword', 
+#      :host => 'todo.dhvg1ytl1sd8.us-east-1.rds.amazonaws.com' 
+#    }
+#  when 'env2'
+#    default[:todo] = { 
+#      :adapter => 'mysql', 
+#      :database => 'todoproduction', 
+#      :username => 'engineyard', 
+#      :password => 'mypassword', 
+#      :host => 'todo.prod.us-east-1.rds.amazonaws.com' 
+#    }
+#  end
