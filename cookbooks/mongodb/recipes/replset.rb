82d3e4902796a4b794480751b262d15e77df7947
Tyler Poland <tyler.poland@gmail.com>
2015-12-01 10:19:44 -0500

    adding changes to manage replica sets better, alpha stage

--- cookbooks/mongodb/recipes/replset.rb
+++ cookbooks/mongodb/recipes/replset.rb
@@ -7,18 +7,19 @@ if @node[:mongo_replset]
 
   # Chef::Log.info "Setting up Replica set: #{node[:mongo_replset]} \n mongo_nodes: #{mongo_nodes.inspect} \n executing on node #{@node[:name]}"
 
-  if (@node[:name].match(/_1$/) && (mongo_nodes.length == 3 || (mongo_nodes.length ==2 && mongo_arbiter.length > 3)))
-    setup_js = "#{node[:mongo_base]}/setup_replset.js"
-
-    template setup_js do
-      source "setup_replset.js.erb"
+  if (mongo_nodes.length >= 3 || (mongo_nodes.length ==2 && mongo_arbiter.length > 3))
+    setup_rb = "#{node[:mongo_base]}/setup_replset.rb"
+    
+    template setup_rb do
+      source "setup_replset.rb.erb"
       owner user[:username]
       group user[:username]
       mode '0755'
-      variables({ :mongo_replset => @node[:mongo_replset],
-                  :mongo_nodes => mongo_nodes,
+      variables({ :mongo_nodes => mongo_nodes,
                   :mongo_port => @node[:mongo_port],
-                  :mongo_arbiter => mongo_arbiter
+                  :mongo_arbiter => mongo_arbiter,
+                  :mongo_path => @node[:mongo_path],
+                  :me => @node[:name]
                })
     end
 
@@ -30,8 +31,15 @@ if @node[:mongo_replset]
     end
     # ----- configure the set
     execute "setup replset #{@node[:mongo_replset]}" do
-      command "#{@node[:mongo_path]}/bin/mongo local #{setup_js}"
+      command "#{setup_rb}"
       only_if "echo 'rs.status()' | #{@node[:mongo_path]}/bin/mongo local --quiet | grep -q 'run rs.initiate'"
+      # <!!!!------ need to expand on this so that this will also run in a cluster that is booting from a snapshot
+      ## > rs.status()
+      ## {
+      ## 	"startupStatus" : 1,
+      ## 	"ok" : 0,
+      ## 	"errmsg" : "loading local.system.replset config (LOADINGCONFIG)"
+      ## }
       Chef::Log.info "Replica set node initialized" 
     end
 
