d03ccdd21c9d819c3f44846bc68c518603246b35
Tyler Poland <tyler.poland@gmail.com>
2016-02-21 00:27:20 -0500

    prevent multiple bind mounts in /proc/mounts

--- cookbooks/elasticsearch/recipes/default.rb
+++ cookbooks/elasticsearch/recipes/default.rb
@@ -102,11 +102,15 @@ if ['util'].include?(node[:instance_role])
       recursive true
     end
 
-    mount "/usr/lib/elasticsearch-#{node[:elasticsearch_version]}/data" do
-      device "#{node[:elasticsearch_home]}"
-      fstype "none"
-      options "bind,rw"
-      action :mount
+    if File.foreach("/proc/mounts").any?{|line| line['/usr/lib[0-9]*/elasticsearch-#{node[:elasticsearch_version]}/data']}
+      Chef::Log.info("Elastic search bind already complete")
+    else
+      mount "/usr/lib/elasticsearch-#{node[:elasticsearch_version]}/data" do
+        device "#{node[:elasticsearch_home]}"
+        fstype "none"
+        options "bind,rw"
+        action :mount
+      end
     end
 
     template "/usr/lib/elasticsearch-#{node[:elasticsearch_version]}/config/logging.yml" do
