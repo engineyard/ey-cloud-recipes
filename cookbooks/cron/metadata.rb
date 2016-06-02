ccbc86f94db1154e5a910bbb537b48efe4765ad6
Christopher Rigor <crigor@gmail.com>
2016-06-02 22:21:32 +0000

    Add cron metadata

---
+++ cookbooks/cron/metadata.rb
@@ -0,0 +1,6 @@
+name 'cron'
+description 'Add cron jobs to instances'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer 'Engine Yard'
+maintainer_email 'info@engineyard.com'
+version '0.0.1'
