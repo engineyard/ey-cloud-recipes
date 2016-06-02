dac3f607fd28dc396b9040ff77533ffc6125995c
Radamanthus Batnag <radamanthus@gmail.com>
2016-06-02 16:34:41 +0800

    Add metadata for solr

---
+++ cookbooks/solr/metadata.rb
@@ -0,0 +1,6 @@
+name 'solr'
+description 'Configuration & deployment of Solr on Engine Yard'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer 'Engine Yard'
+maintainer_email 'support@engineyard.com'
+version '0.0.1'
