f562df58004cdcd329a12a47f89eeb3deca05bf2
Radamanthus Batnag <radamanthus@gmail.com>
2016-06-07 15:47:10 +0800

    Add metadata for Resque and rename README

---
+++ cookbooks/resque/metadata.rb
@@ -0,0 +1,6 @@
+name 'resque'
+description 'Configuration & deployment of Resque on Engine Yard'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer 'Engine Yard'
+maintainer_email 'support@engineyard.com'
+version '0.0.1'
