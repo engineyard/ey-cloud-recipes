f7929a315e935ef6db2db54f5803c136aacde637
Radamanthus Batnag <radamanthus@gmail.com>
2016-06-07 16:46:51 +0800

    Add metadata for collectd, fix sample code

---
+++ cookbooks/collectd/metadata.rb
@@ -0,0 +1,6 @@
+name 'collectd'
+description 'A recipe for tuning collectd alert levels on Engine Yard Cloud'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer 'Engine Yard'
+maintainer_email 'support@engineyard.com'
+version '0.0.1'
