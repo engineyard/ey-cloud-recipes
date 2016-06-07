cbd414f0841d1f145c1cb3dbc3e3084b6f83a0d6
Radamanthus Batnag <radamanthus@gmail.com>
2016-06-07 16:18:47 +0800

    Add metadata for api-keys-yml recipe

---
+++ cookbooks/api-keys-yml/metadata.rb
@@ -0,0 +1,6 @@
+name 'api-keys-yml'
+description "Recipe for setting up production API keys, similar to Rails' secrets.yml, but for non Rails-4+ applications"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer 'Engine Yard'
+maintainer_email 'support@engineyard.com'
+version '0.0.1'
