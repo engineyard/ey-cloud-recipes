67975704d043a357e53f847bd73dc560dd48c0a0
Radamanthus Batnag <radamanthus@gmail.com>
2016-06-10 16:26:46 +0800

    Add metadata for redis-yml

---
+++ cookbooks/redis-yml/metadata.rb
@@ -0,0 +1,6 @@
+name 'redis-yml'
+description 'Recipe for populating redis.yml for running Redis on Engine Yard'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer 'Engine Yard'
+maintainer_email 'support@engineyard.com'
+version '0.0.1'
