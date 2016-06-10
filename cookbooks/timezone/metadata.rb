f8bee22c817657e6ae20ffeaa2086db8437d7915
Radamanthus Batnag <radamanthus@gmail.com>
2016-06-10 16:16:19 +0800

    Add metadata for timezone. Fix notifies syntax to satisfy FC043

---
+++ cookbooks/timezone/metadata.rb
@@ -0,0 +1,6 @@
+name 'timezone'
+description 'Configuration & deployment of timezone on Engine Yard'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer 'Engine Yard'
+maintainer_email 'support@engineyard.com'
+version '0.0.1'
--- cookbooks/timezone/recipes/default.rb
+++ cookbooks/timezone/recipes/default.rb
@@ -10,6 +10,8 @@ service "nginx"
 
 link "/etc/localtime" do
   to "/usr/share/zoneinfo/#{timezone}"
-  notifies :restart, resources(:service => ["vixie-cron", "sysklogd", "nginx"]), :delayed
+  notifies :restart, resources("service[vixie-cron]"), :delayed
+  notifies :restart, resources("service[sysklogd]"), :delayed
+  notifies :restart, resources("service[nginx]"), :delayed
   not_if "readlink /etc/localtime | grep -q '#{timezone}$'"
 end
