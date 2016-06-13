1ff28df3854f0e7ef26b294df75d90051328e144
Radamanthus Batnag <radamanthus@gmail.com>
2016-06-13 16:12:51 +0800

    Add metadata for packages recipe

---
+++ cookbooks/packages/metadata.rb
@@ -0,0 +1,6 @@
+name 'packages'
+description 'Custom chef recipe for installing Gentoo packages on Engine Yard Cloud'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer 'Engine Yard'
+maintainer_email 'support@engineyard.com'
+version '0.0.1'
