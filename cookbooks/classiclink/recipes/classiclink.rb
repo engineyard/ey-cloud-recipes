86b3c2f9e8dd7fe1a810ae91e7a2df69b73798f6
Paul Tingle <github@7lb.co.uk>
2016-10-06 16:35:55 +0100

    update recipe so instance links itself

--- cookbooks/classiclink/recipes/classiclink.rb
+++ cookbooks/classiclink/recipes/classiclink.rb
@@ -3,11 +3,7 @@
 # Recipe:: classiclink
 #
 
-if solo? || app_master?
-  node[:engineyard][:environment][:instances].each do |i|
-    id = i.first[1]
-    bash "attach-classiclink-for-#{id}" do
-      code "aws ec2 attach-classic-link-vpc --instance-id #{id} --vpc-id #{node[:classiclink_vpc_id]} --groups #{node[:classiclink_security_group_id]}"
-    end
-  end
+id = node[:engineyard][:this]
+bash "attach-classiclink-for-#{id}" do
+  code "aws ec2 attach-classic-link-vpc --instance-id #{id} --vpc-id #{node[:classiclink_vpc_id]} --groups #{node[:classiclink_security_group_id]}"
 end
