11f6d444a6542b814412bbd8b92ec6cdbd22f78a
Radamanthus Batnag <radamanthus@gmail.com>
2017-02-28 12:37:00 +0800

    Use IcedTea 7.2.6.8 for Solr

--- cookbooks/solr/attributes/solr.rb
+++ cookbooks/solr/attributes/solr.rb
@@ -1,5 +1,5 @@
 default['solr'] = {
-  'java_version'          => '7.2.3.3-r1',
+  'java_version'          => '7.2.6.8',
   'java_eselect_version'  => 'icedtea-bin-7',
   'solr_version'          => '5.5.2',
   'core_name'             => 'default',
