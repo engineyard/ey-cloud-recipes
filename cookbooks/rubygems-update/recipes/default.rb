94c2d7d0d01de644ddc4a323d336c197d7712ad3
Paul Tingle <github@7lb.co.uk>
2019-03-22 10:13:51 +0000

    Updates rubygems update to 2.7.8

--- cookbooks/rubygems-update/recipes/default.rb
+++ cookbooks/rubygems-update/recipes/default.rb
@@ -1,3 +1,3 @@
-execute "Update to rubygems 2.6.13" do
-  command "gem install -v 2.6.13 rubygems-update && update_rubygems"
+execute "Update to rubygems 2.7.8" do
+  command "gem install -v 2.7.8 rubygems-update && update_rubygems"
 end
