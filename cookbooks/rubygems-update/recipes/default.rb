bfe1437f31833c829ba548c92a8f5f26ba477c21
Paul Tingle <github@7lb.co.uk>
2019-06-07 12:43:31 +0100

    Update default.rb
    Adding commands to remove packaged bundler.

--- cookbooks/rubygems-update/recipes/default.rb
+++ cookbooks/rubygems-update/recipes/default.rb
@@ -1,3 +1,6 @@
 execute "Update to rubygems 2.7.9" do
   command "gem install -v 2.7.9 rubygems-update && update_rubygems"
 end
+execute "Remove bundler installed by rubygems" do
+  command "rm -rf /usr/lib64/ruby/site_ruby/*/bundler{,.rb} && rm -f /usr/local/lib64/ruby/gems/*/specifications/default/bundler*gemspec"
+end
