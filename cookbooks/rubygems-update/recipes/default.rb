be0c79194f3e2193288d44396ceb53db2a09fb11
Paul Tingle <github@7lb.co.uk>
2019-06-07 10:29:37 +0100

    Update default.rb
    Updating rubygem version from 2.7.8 to 2.7.9.

--- cookbooks/rubygems-update/recipes/default.rb
+++ cookbooks/rubygems-update/recipes/default.rb
@@ -1,3 +1,3 @@
-execute "Update to rubygems 2.7.8" do
-  command "gem install -v 2.7.8 rubygems-update && update_rubygems"
+execute "Update to rubygems 2.7.9" do
+  command "gem install -v 2.7.9 rubygems-update && update_rubygems"
 end
